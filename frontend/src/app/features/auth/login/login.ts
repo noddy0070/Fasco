c01b6207b523c7ddce303224da71cee3bcad1965
utkarshSaxena <utkarsh_saxena@epam.com>
2026-04-04 01:50:50 +0530

    changes

--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -1,17 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { BlackButton } from "../../../shared/components/black-button/black-button";
 import { TransitionLink } from "../../../shared/components/transition-link/transition-link";
 import { AuthFrame } from "../../../layout/auth-frame/auth-frame";
+import { EyeTrack } from "../../../shared/components/eye-track/eye-track";
 
 @Component({
   selector: 'app-login',
-  imports: [BlackButton, TransitionLink, AuthFrame],
+  imports: [BlackButton, TransitionLink, AuthFrame, EyeTrack],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login{
-  showPassword = false;
-  togglePassword() {
-    this.showPassword = !this.showPassword;
-  }
+  isEyeClosed = signal(true);
+  
 }
