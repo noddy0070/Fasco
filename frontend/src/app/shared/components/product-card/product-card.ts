0cde4e2b773f9dfef26ed5e83d731e9978e1da1d
utkarshSaxena <utkarsh_saxena@epam.com>
2026-05-07 22:55:48 +0530

    created product card

--- frontend/src/app/shared/components/product-card/product-card.ts
+++ frontend/src/app/shared/components/product-card/product-card.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { input } from "@angular/core";
+import { Component, input } from '@angular/core';
+
+export interface ProductCardItem {
+  name: string;
+  variant: string;
+  price: string;
+  priceValue: number;
+  image: string;
+  badge: string;
+  moreColors: string;
+  swatches: string[];
+  sizes: string[];
+  colors: string[];
+  productType: string;
+  material: string;
+}
+
 @Component({
   selector: 'app-product-card',
+  imports: [],
   templateUrl: './product-card.html',
   styleUrl: './product-card.css',
 })
 export class ProductCard {
-  title=input<string>('')
-  price=input<string>('')
-  url=input<string>('')
-
+  product = input.required<ProductCardItem>();
 }
